15183084cf8e5a479902768820677505dce7aabc
Lester Laurence Z. Corpuz <lester19.corpuz28@gmail.com>
2022-02-03 22:37:33 +0800

    updated back button
    navigator push function to go back to the pet homepage

--- lib/projects/superheroes/ui/superhero_slider_page.dart
+++ lib/projects/superheroes/ui/superhero_slider_page.dart
@@ -4,13 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_projects/projects/instagram_redesign/cubit/pet_cubit.dart';
 import 'package:flutter_projects/projects/superheroes/models/superhero.dart';
+import 'package:flutter_projects/projects/superheroes/ui/pet_homepage.dart';
 import 'package:flutter_projects/projects/superheroes/ui/superhero_detail_page.dart';
 import 'package:flutter_projects/projects/superheroes/ui/widgets/superhero_card.dart';
 
-
 class SuperheroSliderPage extends StatefulWidget {
-  const SuperheroSliderPage({Key? key,  }) : super(key: key);
-  
+  const SuperheroSliderPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   _SuperheroSliderPageState createState() => _SuperheroSliderPageState();
@@ -23,7 +24,6 @@ class _SuperheroSliderPageState extends State<SuperheroSliderPage> {
   double? _percent;
   double? _auxPercent;
   late bool _isScrolling;
-
 
   @override
   void initState() {
@@ -61,102 +61,108 @@ class _SuperheroSliderPageState extends State<SuperheroSliderPage> {
 
   @override
   Widget build(BuildContext context) {
-    const  angleRotate = -pi * .5;
+    const angleRotate = -pi * .5;
     return Scaffold(
       //---------------
       // App Bar
       //---------------
       appBar: AppBar(
-        title:const  Text("Pets"),
+        title: const Text("Pets"),
         elevation: 0,
         centerTitle: true,
         leading: IconButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const PetHomePage()),
+            );
+          },
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
       body: BlocBuilder<PetCubit, PetState>(
-        builder: (BuildContext context, PetState state) {
-          if(state is PetSuccessfulGet){
-            final List<Map<String, dynamic>>pets = state.entries;
-            return Stack(
-              children: [
-                //-----------------------
-                // Superhero Cards
-                //-----------------------
-                AnimatedPositioned(
-                  duration: kThemeAnimationDuration,
-                  top: 0,
-                  bottom: 0,
-                  right: _isScrolling ? 10 : 0,
-                  left: _isScrolling ? 10 : 0,
-                  child: Stack(
-                    children: [
-                      //----------------
-                      // Back Card
-                      //----------------
-                      Transform.translate(
-                        offset: Offset(0, 50 * _auxPercent!),
+          builder: (BuildContext context, PetState state) {
+        if (state is PetSuccessfulGet) {
+          final List<Map<String, dynamic>> pets = state.entries;
+          return Stack(
+            children: [
+              //-----------------------
+              // Superhero Cards
+              //-----------------------
+              AnimatedPositioned(
+                duration: kThemeAnimationDuration,
+                top: 0,
+                bottom: 0,
+                right: _isScrolling ? 10 : 0,
+                left: _isScrolling ? 10 : 0,
+                child: Stack(
+                  children: [
+                    //----------------
+                    // Back Card
+                    //----------------
+                    Transform.translate(
+                      offset: Offset(0, 50 * _auxPercent!),
+                      child: PetCard(
+                        pet: pets[_auxIndex.clamp(0, pets.length - 1)],
+                        factorChange: _auxPercent,
+                      ),
+                    ),
+                    //----------------
+                    // Front Card
+                    //----------------
+                    Transform.translate(
+                      offset: Offset(-800 * _percent!, 100 * _percent!),
+                      child: Transform.rotate(
+                        angle: angleRotate * _percent!,
                         child: PetCard(
-                          pet: pets[_auxIndex.clamp(0, pets.length - 1)],
-                          factorChange: _auxPercent,
+                          pet: pets[_index],
+                          factorChange: _percent,
                         ),
                       ),
-                      //----------------
-                      // Front Card
-                      //----------------
-                      Transform.translate(
-                        offset: Offset(-800 * _percent!, 100 * _percent!),
-                        child: Transform.rotate(
-                          angle: angleRotate * _percent!,
-                          child: PetCard(
-                            pet: pets[_index],
-                            factorChange: _percent,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
-                //-----------------------------------------------------
-                // VOID PAGE VIEW
-                // This page view is just for using the page controller
-                //-----------------------------------------------------
-                PageView.builder(
-                  controller: _pageController,
-                  itemCount: pets.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () => _openDetail(context, pets[index]),
-                      child: const SizedBox(),
-                    );
-                  },
-                )
-              ],
-            );
-          }
-          else{
-            return Center(
-              child: CircularProgressIndicator(
-                color: Colors.yellow[800],
               ),
-            );
-          }
+              //-----------------------------------------------------
+              // VOID PAGE VIEW
+              // This page view is just for using the page controller
+              //-----------------------------------------------------
+              PageView.builder(
+                controller: _pageController,
+                itemCount: pets.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () => _openDetail(context, pets[index]),
+                    child: const SizedBox(),
+                  );
+                },
+              )
+            ],
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.yellow[800],
+            ),
+          );
         }
-      ),
+      }),
     );
   }
 
   void _openDetail(BuildContext context, Map<String, dynamic> pet) {
-    Navigator.push(context, PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) {
-        return FadeTransition(
-          opacity: animation,
-          child: SuperheroDetailPage(
-            pet: pet,
-          ),
-        );
-      },
-    ),);
+    Navigator.push(
+      context,
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return FadeTransition(
+            opacity: animation,
+            child: SuperheroDetailPage(
+              pet: pet,
+            ),
+          );
+        },
+      ),
+    );
   }
 }
